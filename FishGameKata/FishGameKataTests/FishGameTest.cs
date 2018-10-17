0f88f685d85e477fc45c4325d8768d243ec247a4
Maarwaa <marwa.brahem@esprit.tn>
2018-10-17 06:37:07 +0100

    Add some code

---
+++ FishGameKata/FishGameKataTests/FishGameTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using FishsGameKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FishGameKataTests
+{
+    [TestClass]
+    public class FishGameTest
+    {
+        [TestMethod]
+        public void FishTest1()
+        {
+            GameFish game = new GameFish();
+            string fishs = "";
+            Assert.AreEqual(1, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest2()
+        {
+            GameFish game = new GameFish();
+            string fishs = "0";
+            Assert.AreEqual(1, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest3()
+        {
+            GameFish game = new GameFish();
+            string fishs = "6";
+            Assert.AreEqual(1, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest4()
+        {
+            GameFish game = new GameFish();
+            string fishs = "1111";
+            Assert.AreEqual(2, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest5()
+        {
+            GameFish game = new GameFish();
+            string fishs = "11112222";
+            Assert.AreEqual(3, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest6()
+        {
+            GameFish game = new GameFish();
+            string fishs = "111122223333";
+            Assert.AreEqual(4, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest7()
+        {
+            GameFish game = new GameFish();
+            string fishs = "111111111111";
+            Assert.AreEqual(3, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest8()
+        {
+            GameFish game = new GameFish();
+            string fishs = "111111111111111111112222222222";
+            Assert.AreEqual(5, game.fish(fishs));
+        }
+
+        [TestMethod]
+        public void FishTest9()
+        {
+            GameFish game = new GameFish();
+            string fishs = "151128241212192113722321331";
+            Assert.AreEqual(5, game.fish(fishs));
+        }
+    }
+}
---
+++ FishGameKata/FishsGameKata/GameFish.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishsGameKata
+{
+    public class GameFish
+    {
+        public int fish(string fishs)
+        {
+            int[] mytab = RemplirTab(fishs);
+            int init = 1;
+            bool x = Tabnull(mytab);
+            while (x == false)
+            {
+                switch (init)
+                {
+
+                    case 1:
+                        if (mytab[1] >= 4)
+                        {
+                            init++;
+                            mytab[1] = mytab[1] - 4;
+                        }
+
+                        break;
+
+                    case 2:
+                        if (mytab[1] >= 8)
+                        {
+                            init++;
+                            mytab[1] = mytab[1] - 8;
+                        }
+                        if (mytab[2] >= 4)
+                        {
+                            init++;
+                            mytab[2] = mytab[2] - 4;
+                        }
+
+                        break;
+                    case 3:
+                        if (mytab[1] >= 12)
+                        {
+                            init++;
+                            mytab[1] = mytab[1] - 12;
+                        }
+                        if (mytab[2] >= 6)
+                        {
+                            init++;
+                            mytab[2] = mytab[2] - 6;
+                        }
+                        if (mytab[3] >= 4)
+                        {
+                            init++;
+                            mytab[3] = mytab[3] - 4;
+                        }
+                        break;
+                    case 4:
+                        if (mytab[1] >= 16)
+                        {
+                            init++;
+                            mytab[1] = mytab[1] - 16;
+                        }
+                        if (mytab[2] >= 8)
+                        {
+                            init++;
+                            mytab[2] = mytab[2] - 8;
+                        }
+                        if (mytab[4] >= 4)
+                        {
+                            init++;
+                            mytab[4] = mytab[4] - 4;
+                        }
+                        break;
+                    case 5:
+                        if (mytab[1] >= 20)
+                        {
+                            init++;
+                            mytab[1] = mytab[1] - 20;
+                        }
+                        if (mytab[2] >= 10)
+                        {
+                            init++;
+                            mytab[2] = mytab[2] - 10;
+                        }
+                        if (mytab[4] >= 5)
+                        {
+                            init++;
+                            mytab[4] = mytab[4] - 5;
+                        }
+                        if (mytab[5] >= 4)
+                        {
+                            init++;
+                            mytab[5] = mytab[5] - 4;
+                        }
+                        break;
+                    case 6:
+                        if (mytab[1] >= 24)
+                        {
+                            init++;
+                            mytab[1] = mytab[1] - 24;
+                        }
+                        if (mytab[2] >= 12)
+                        {
+                            init++;
+                            mytab[2] = mytab[2] - 12;
+                        }
+                        if (mytab[3] >= 8)
+                        {
+                            init++;
+                            mytab[3] = mytab[3] - 8;
+                        }
+                        if (mytab[4] >= 6)
+                        {
+                            init++;
+                            mytab[4] = mytab[4] - 6;
+                        }
+                        if (mytab[6] >= 4)
+                        {
+                            init++;
+                            mytab[6] = mytab[6] - 4;
+                        }
+                        break;
+                }
+            }
+            return init;
+        }
+        public bool Tabnull(int[] tabl)
+        {
+            bool nul = true;
+            int i = 0;
+
+            while (i < 10 && nul == true)
+            {
+                if (tabl[i] == 0)
+                {
+                    nul = true;
+                    i++;
+                }
+                else
+                    nul = false;
+
+            }
+            return nul;
+        }
+        public int[] RemplirTab(string word)
+        {
+            int[] mytabl = new int[10]; ;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (word.Contains(i.ToString()))
+                        mytabl[i] = mytabl[i] + 1;
+
+                }
+            }
+            return mytabl;
+
+        }
+    }
+}
